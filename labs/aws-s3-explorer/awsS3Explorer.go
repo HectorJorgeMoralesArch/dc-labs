9749eb3f3776991f49cd61021277aa08c92ad7e1
Hector Jorge Morales Arch <hjma98@gmail.com>
2021-03-16 05:14:03 +0000

    Started the Search Function
    Signed-off-by: Hector Jorge Morales Arch <hjma98@gmail.com>

--- labs/aws-s3-explorer/awsS3Explorer.go
+++ labs/aws-s3-explorer/awsS3Explorer.go
@@ -2,6 +2,7 @@ package main
 import (
 	"encoding/xml"
 	"fmt"
+	"log"
 	"io/ioutil"
 	"net/http"
 	"os"
@@ -32,6 +33,29 @@ type ListBucket struct {
 	XMLName  xml.Name  `xml:"ListBucketResult"`
 	Contents []Content `xml:"Contents"`
 }
+func search(w http.ResponseWriter, r *http.Request)
+{
+	max:=5
+	exts := make(map[string]int)
+	dirs := make(map[string]bool)
+	objs := make(map[string]bool)
+	
+	resp, err := http.Get(fmt.Sprintf("https://%v.s3.amazonaws.com", r.FormValue(BName)))
+	if err!=""{
+		log.Fatalln(err)
+	}
+}
 func main() {
-
+	var p = flag.Int("port", 9876, "Port")
+	flag.Parse()
+	s := fmt.Sprintf("localhost:%v", *p)
+	http.HandleFunc("/bar", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+		search(w, r)
+    })
+	
+	err := http.ListenAndServe(socket, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
