f3db96db8373b16e120d69e0ec02f685161cbf12
Hector Jorge Morales Arch <hjma98@gmail.com>
2021-03-16 05:13:28 +0000

    Client
    Signed-off-by: Hector Jorge Morales Arch <hjma98@gmail.com>

--- labs/aws-s3-explorer/awsS3Client.go
+++ labs/aws-s3-explorer/awsS3Client.go
@@ -1,4 +1,32 @@
 package main
 
+import (
+	"bufio"
+	"log"
+	"fmt"
+	"os"
+	"net/http"
+	"flag"
+)
+
 func main() {
+
+	var P = flag.String("proxy", "localhost:9543", "Proxy")
+	var BName = flag.String("bucket", "", "Bucket name")
+	var Dir = flag.String("directory", "", "Directory")
+	flag.Parse()
+	if *BName == "" {
+		log.Fatalln("Incorrect Argument")
+	}
+	request := fmt.Sprintf("http://%v/example?bucket=%v&dir=%v", *P, *BName, *Dir)
+	resp, err := http.Get(request)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	
+	S := bufio.NewScanner(resp.Body)
+	for i := 0; S.Scan(); i++ {
+		log.Println(S.Text())
+	}
+	resp.Body.Close()
 }
