9101d882efec111a6a69e793943f5fbaa78d4fb7
Hector Jorge Morales Arch <hjma98@gmail.com>
2021-03-16 04:29:10 +0000

    Data Structures
    Signed-off-by: Hector Jorge Morales Arch <hjma98@gmail.com>

--- labs/aws-s3-explorer/awsS3Explorer.go
+++ labs/aws-s3-explorer/awsS3Explorer.go
@@ -1,4 +1,19 @@
 package main
 
+//General Data Structure
+type GenData struct {
+	BName string
+	Objs map[string]bool
+	Dirs map[string]bool
+	Exts map[string]int
+}
+//Specific Data Structure
+type GenData struct {
+	SName string
+	Objs map[string]bool
+	Dirs map[string]bool
+	Exts map[string]int
+}
 func main() {
+	
 }
